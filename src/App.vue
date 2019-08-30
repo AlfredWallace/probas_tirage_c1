b31481fee3d54208cf68cd7412801eafaec6858c
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2019-08-30 19:37:35 +0200

    Quick & dirty, but it works

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="team in allowedPicks(this.teams.filter(teamToFilter => teamToFilter.name === 'Chelsea')[0])" :key="team.name">
-        {{ team.name }}
-      </li>
-    </ul>
+    <table border="1">
+      <thead>
+      <th></th>
+      <th v-for="winner in winners">{{ winner.name }}</th>
+      </thead>
+      <tbody>
+      <tr v-for="runnerUp in runnersUp">
+        <td>{{ runnerUp.name }}</td>
+        <td v-for="winner in winners">{{ getPercentage(winner, runnerUp) }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <button @click="draw">Run</button>
   </div>
 </template>
 
@@ -15,44 +23,90 @@ export default {
   name: 'app',
   data() {
     return {
-      teams: [
-        new Team('ManU', 'Angleterre', 'A', 'winner', 3),
-        new Team('PSG', 'France', 'B', 'winner', 1),
-        new Team('Roma', 'Italie', 'C', 'winner', 6),
-        new Team('Barca', 'Espagne', 'D', 'winner', 0),
-        new Team('Liverpool', 'Angleterre', 'E', 'winner', 5),
-        new Team('ManCity', 'Angleterre', 'F', 'winner', 2),
-        new Team('Besiktas', 'Turquie', 'G', 'winner', 7),
-        new Team('Tottenham', 'Angleterre', 'H', 'winner', 4),
-        new Team('Bâle', 'Suisse', 'A', 'runner-up', 7),
-        new Team('Bayern', 'Allemagne', 'B', 'runner-up', 1),
-        new Team('Chelsea', 'Angleterre', 'C', 'runner-up', 4),
-        new Team('Juve', 'Italie', 'D', 'runner-up', 2),
-        new Team('Séville', 'Espagne', 'E', 'runner-up', 3),
-        new Team('Chakhtar', 'Ukraine', 'F', 'runner-up', 6),
-        new Team('Porto', 'Portugal', 'G', 'runner-up', 5),
-        new Team('Real', 'Espagne', 'H', 'runner-up', 0),
+      nbDraws: 100000,
+      winners: [
+        new Team('ManU', 'Angleterre', 'A', 3),
+        new Team('PSG', 'France', 'B', 1),
+        new Team('Roma', 'Italie', 'C', 6),
+        new Team('Barca', 'Espagne', 'D', 0),
+        new Team('Liverpool', 'Angleterre', 'E', 5),
+        new Team('ManCity', 'Angleterre', 'F', 2),
+        new Team('Besiktas', 'Turquie', 'G', 7),
+        new Team('Tottenham', 'Angleterre', 'H', 4),
+      ],
+      runnersUp: [
+        new Team('Bâle', 'Suisse', 'A', 7),
+        new Team('Bayern', 'Allemagne', 'B', 1),
+        new Team('Chelsea', 'Angleterre', 'C', 4),
+        new Team('Juve', 'Italie', 'D', 2),
+        new Team('Séville', 'Espagne', 'E', 3),
+        new Team('Chakhtar', 'Ukraine', 'F', 6),
+        new Team('Porto', 'Portugal', 'G', 5),
+        new Team('Real', 'Espagne', 'H', 0),
       ]
     }
   },
-  computed: {
-    winners() {
-      return this.teams.filter(team => team.group === 'winner')
-    },
-    runnersUp() {
-      return this.teams.filter(team => team.group === 'runner-up')
-    },
-  },
+  computed: {},
   methods: {
     draw() {
-      // copying the groups
-      const winners = this.winners().slice(0)
-      const runnersUp = this.runnersUp().slice(0)
+      this.resetPicks()
 
+      let cpt = 0
+      let test = 0
+
+      draw: while (cpt < this.nbDraws) {
+
+        // copying the groups
+        let localWinners = this.winners.slice(0)
+        let localRunnersUp = this.runnersUp.slice(0)
+        let localDraw = []
+
+        for (let i = 0; i < localWinners.length; i++) {
+          const pickedIndex = Math.floor(Math.random() * localRunnersUp.length)
+          const runnerUp = localRunnersUp[pickedIndex]
+          const winner = localWinners[i]
+
+          if (runnerUp.country === winner.country || runnerUp.pool === winner.pool) {
+            test++
+            continue draw
+          }
+
+          localDraw[i] = runnerUp.name
+          localRunnersUp.splice(pickedIndex, 1)
+        }
+
+        for (let j = 0; j < localWinners.length; j++) {
+          localWinners[j].allowedPicks.filter(pick => pick.name === localDraw[j])[0].timesPicked++
+        }
+
+        cpt++
+      }
     },
     allowedPicks(team) {
-      return this.teams.filter(teamToPick => teamToPick.group !== team.group && teamToPick.country !== team.country && teamToPick.pool !== team.pool)
+      return this.runnersUp
+        .filter(teamToFilter => teamToFilter.country !== team.country && teamToFilter.pool !== team.pool)
+        .map(teamToMap => {
+          return {
+            name: teamToMap.name,
+            timesPicked: 0
+          }
+        })
+    },
+    getPercentage(winner, runnerUp) {
+      let picks = winner.allowedPicks.filter(pick => pick.name === runnerUp.name);
+      if (picks.length) {
+        return (picks[0].timesPicked / this.nbDraws * 100).toFixed(2)
+      }
+      return ''
+    },
+    resetPicks() {
+      for (let i = 0; i < this.winners.length; i++) {
+        this.winners[i].allowedPicks = this.allowedPicks(this.winners[i])
+      }
     }
+  },
+  created() {
+    this.resetPicks()
   }
 }
 </script>
